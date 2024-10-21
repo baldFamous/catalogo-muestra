21107f8cb873238f6170f3cb4980961b8799e295
brodriguez <bastian3967a@gmail.com>
2024-10-20 22:30:56 -0300

    delete about in nav

--- catalogo-frontend/src/app/layout.js
+++ catalogo-frontend/src/app/layout.js
@@ -5,8 +5,8 @@ import Footer from "@/components/footer/footer";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Catalogo XXXX",
-  description: "Catalogo de productos de la tienda XXXX",
+  title: "Catalogo",
+  description: "Catalogo de productos de la tienda",
 };
 
 export default function RootLayout({ children, modal}) {
@@ -14,8 +14,8 @@ export default function RootLayout({ children, modal}) {
     <html lang="es">
     <body className={montserrat.className}>
     <nav className="nav-bar">
-        <ul><a href="/">NegocioXXXX</a></ul>
-        <ul><a href="/about">Sobre nosotros</a></ul>
+        <ul><a href="/">Negocio</a></ul>
+        {/* <ul><a href="/about">Sobre nosotros</a></ul> */}
     </nav>
     {children}
     {modal}
